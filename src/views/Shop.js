b4ec353af938e1ace9f6cf0744e8cb3b91dd161a
augustoalejandro95@gmail.com <avaca@romacc.com>
2021-11-16 17:24:47 -0500

    profile add blurry

--- src/views/Shop.js
+++ src/views/Shop.js
@@ -28,6 +28,8 @@ import work3 from "assets/img/examples/cynthia-del-rio.jpg";
 import work4 from "assets/img/examples/mariya-georgieva.jpg";
 import work5 from "assets/img/examples/clem-onojegaw.jpg";
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
+import BackdropFilter from "react-backdrop-filter";
+import "views/styles.css";
 
 import useClasses from "components/UseClasses";
 
@@ -49,332 +51,36 @@ const Shop = (props) => {
               small
               filter
               image={require("assets/img/profile-bg.jpg").default}
-            />
-             <div className={classNames(classes.main, classes.mainRaised)}>
-              <div>
-                <div className={classes.container}>
-                  <GridContainer direction="column"
-                     alignItems="center"
-                     justifyContent="center" 
-                     justify="center">
-                    <GridItem xs={12} sm={12} md={6}>
-                      <div className={classes.profile}>
-                        <div>
-                          <img src={profile} alt="..." className={imageClasses} />
-                        </div>
-                        <div className={classes.name}>
-                          <h3 className={classes.title}>Christian Louboutin</h3>
-                          <h6>DESIGNER</h6>
-                          {/* <Button justIcon link className={classes.margin5}>
-                            <i className={"fab fa-twitter"} />
-                          </Button>
-                          <Button justIcon link className={classes.margin5}>
-                            <i className={"fab fa-instagram"} />
-                          </Button>
-                          <Button justIcon link className={classes.margin5}>
-                            <i className={"fab fa-facebook"} />
-                          </Button> */}
-                        </div>
-                      </div>
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={6}>
-                     <div className={classes.description}>
-                         <p>
-                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                         when an unknown printer took a galley of type and scrambled it to make a type 
-                         specimen book. It has survived not only five centuries, but also the leap into 
-                         electronic typesetting, remaining essentially unchanged. It was popularised in 
-                         the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.{" "}
-                         </p>
-                     </div>
-                    </GridItem>
-                    
-                  </GridContainer>
-                  
-                  <GridContainer justify="center">
-                    <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                      {/* <NavPills
-                        alignCenter
-                        color="primary"
-                        tabs={[
-                          {
-                            tabButton: "Studio",
-                            tabIcon: Camera,
-                            tabContent: (
-                              <GridContainer justify="center">
-                                <GridItem xs={12} sm={12} md={4}>
-                                  <img
-                                    alt="..."
-                                    src={studio1}
-                                    className={navImageClasses}
-                                  />
-                                  <img
-                                    alt="..."
-                                    src={studio2}
-                                    className={navImageClasses}
-                                  />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={4}>
-                                  <img
-                                    alt="..."
-                                    src={studio5}
-                                    className={navImageClasses}
-                                  />
-                                  <img
-                                    alt="..."
-                                    src={studio4}
-                                    className={navImageClasses}
-                                 />
-                               </GridItem>
-                             </GridContainer>
-                           ),
-                         },
-                         {
-                           tabButton: "Work",
-                           tabIcon: Palette,
-                           tabContent: (
-                             <GridContainer justify="center">
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work1}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work2}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work3}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work4}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work5}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                             </GridContainer>
-                           ),
-                         },
-                         {
-                           tabButton: "Favorite",
-                           tabIcon: Favorite,
-                           tabContent: (
-                             <GridContainer justify="center">
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work4}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={studio3}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work2}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work1}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={studio1}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                             </GridContainer>
-                           ),
-                         },
-                       ]}
-                     /> */}
-                   </GridItem>
-                 </GridContainer>
-               </div>
-             </div>
-           </div>
-           <Box sx={{ flexGrow: 6}} />
-           <div className={classNames(classes.main, classes.mainRaised)}>
-              <div>
-                <div className={classes.container}>
-                  <GridContainer direction="column"
-                     alignItems="center"
-                     justifyContent="center" 
-                     justify="center">
-                    <GridItem xs={12} sm={12} md={6}>
-                      <div className={classes.profile}>
-                        <div>
-                          <img src={profile} alt="..." className={imageClasses} />
-                        </div>
-                        <div className={classes.name}>
-                          <h3 className={classes.title}>Christian Louboutin</h3>
-                          <h6>DESIGNER</h6>
-                          {/* <Button justIcon link className={classes.margin5}>
-                            <i className={"fab fa-twitter"} />
-                          </Button>
-                          <Button justIcon link className={classes.margin5}>
-                            <i className={"fab fa-instagram"} />
-                          </Button>
-                          <Button justIcon link className={classes.margin5}>
-                            <i className={"fab fa-facebook"} />
-                          </Button> */}
-                        </div>
-                      </div>
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={6}>
-                     <div className={classes.description}>
-                         <p>
-                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                         when an unknown printer took a galley of type and scrambled it to make a type 
-                         specimen book. It has survived not only five centuries, but also the leap into 
-                         electronic typesetting, remaining essentially unchanged. It was popularised in 
-                         the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.{" "}
-                         </p>
-                     </div>
-                    </GridItem>
-                    
-                  </GridContainer>
-                  
-                  <GridContainer justify="center">
-                    <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                      {/* <NavPills
-                        alignCenter
-                        color="primary"
-                        tabs={[
-                          {
-                            tabButton: "Studio",
-                            tabIcon: Camera,
-                            tabContent: (
-                              <GridContainer justify="center">
-                                <GridItem xs={12} sm={12} md={4}>
-                                  <img
-                                    alt="..."
-                                    src={studio1}
-                                    className={navImageClasses}
-                                  />
-                                  <img
-                                    alt="..."
-                                    src={studio2}
-                                    className={navImageClasses}
-                                  />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={4}>
-                                  <img
-                                    alt="..."
-                                    src={studio5}
-                                    className={navImageClasses}
-                                  />
-                                  <img
-                                    alt="..."
-                                    src={studio4}
-                                    className={navImageClasses}
-                                 />
-                               </GridItem>
-                             </GridContainer>
-                           ),
-                         },
-                         {
-                           tabButton: "Work",
-                           tabIcon: Palette,
-                           tabContent: (
-                             <GridContainer justify="center">
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work1}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work2}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work3}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work4}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work5}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                             </GridContainer>
-                           ),
-                         },
-                         {
-                           tabButton: "Favorite",
-                           tabIcon: Favorite,
-                           tabContent: (
-                             <GridContainer justify="center">
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work4}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={studio3}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                               <GridItem xs={12} sm={12} md={4}>
-                                 <img
-                                   alt="..."
-                                   src={work2}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={work1}
-                                   className={navImageClasses}
-                                 />
-                                 <img
-                                   alt="..."
-                                   src={studio1}
-                                   className={navImageClasses}
-                                 />
-                               </GridItem>
-                             </GridContainer>
-                           ),
-                         },
-                       ]}
-                     /> */}
-                   </GridItem>
-                 </GridContainer>
-               </div>
-             </div>
-           </div>
+            >
+                <div className="container">
+
+                <BackdropFilter
+                className="bluredForm"
+                filter={"blur(5px) sepia(50%)"}
+                html2canvasOpts={{
+                    allowTaint: true
+                }}
+                onDraw={() => {
+                    console.log("Rendered !");
+                }}
+                >
+                    <form>
+                <h4>@tnargib</h4>
+                <p>Login</p>
+                <hr />
+                <p>Password</p>
+                <hr />
+                <div className="button">Sign in</div>
+                <small>Forgot password ?</small>
+                </form>
+                </BackdropFilter>
+                </div>
+            </Parallax>
+            
+    
          </div>
     );
 }
 
 export default Shop;
+
