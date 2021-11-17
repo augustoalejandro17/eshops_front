4a263a88ff2e9a6bbe808865e93dc9884ada04f2
augustoalejandro95@gmail.com <avaca@romacc.com>
2021-11-17 17:23:23 -0500

    cambios provisionales

--- src/views/Shop.js
+++ src/views/Shop.js
@@ -4,32 +4,20 @@ import { useParams } from "react-router-dom";
 import classNames from "classnames";
 // @material-ui/core components
 // @material-ui/icons
-import Camera from "@mui/icons-material/Camera";
-import Palette from "@mui/icons-material/Palette";
-import Favorite from "@mui/icons-material/Favorite";
-// core components
-import Button from "components/CustomButtons/Button.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import NavPills from "components/NavPills/NavPills.js";
+
 import Parallax from "components/Parallax/Parallax.js";
-import Grid from "@mui/material/Grid";
-import { Box } from '@mui/system';
+
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import Button from "components/CustomButtons/Button.js";
+import background from "assets/img/bg2.jpg";
 import profile from "assets/img/faces/christian.jpg";
 
-import studio1 from "assets/img/examples/studio-1.jpg";
-import studio2 from "assets/img/examples/studio-2.jpg";
-import studio3 from "assets/img/examples/studio-3.jpg";
-import studio4 from "assets/img/examples/studio-4.jpg";
-import studio5 from "assets/img/examples/studio-5.jpg";
-import work1 from "assets/img/examples/olu-eletu.jpg";
-import work2 from "assets/img/examples/clem-onojeghuo.jpg";
-import work3 from "assets/img/examples/cynthia-del-rio.jpg";
-import work4 from "assets/img/examples/mariya-georgieva.jpg";
-import work5 from "assets/img/examples/clem-onojegaw.jpg";
+
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
 import BackdropFilter from "react-backdrop-filter";
 import "views/styles.css";
+import Typography from '@mui/material/Typography';
 
 import useClasses from "components/UseClasses";
 
@@ -46,36 +34,48 @@ const Shop = (props) => {
 
     return( 
         <div>
-            <h2>Shop {params.shopIndex}</h2>
-            <Parallax
+            {/* <h2>Shop {params.shopIndex}</h2> */}
+            {/* <Parallax
               small
               filter
-              image={require("assets/img/profile-bg.jpg").default}
-            >
+            //   image={require("assets/img/faces/christian.jpg").default}
+            > */}
+            <Card style={{ backgroundImage: `url(${profile})` }}>
+            <CardBody background >
                 <div className="container">
-
-                <BackdropFilter
-                className="bluredForm"
-                filter={"blur(5px) sepia(50%)"}
-                html2canvasOpts={{
-                    allowTaint: true
-                }}
-                onDraw={() => {
-                    console.log("Rendered !");
-                }}
-                >
-                    <form>
-                <h4>@tnargib</h4>
-                <p>Login</p>
-                <hr />
-                <p>Password</p>
-                <hr />
-                <div className="button">Sign in</div>
-                <small>Forgot password ?</small>
-                </form>
-                </BackdropFilter>
+                    <BackdropFilter
+                    className="bluredContent"
+                    // filter={"blur(100px) gray(50%)"}
+                    html2canvasOpts={{
+                        allowTaint: true
+                    }}
+                    onDraw={() => {
+                        console.log("Rendered !");
+                    }}
+                    >
+                        <Typography class ="text" variant="body1" >
+                            I will be the leader of a company that ends up being worth
+                            billions of dollars, because I got the answers...I will be the leader of a company that ends up being worth
+                            billions of dollars, because I got the answers...I will be the leader of a company that ends up being worth
+                            billions of dollars, because I got the answers...I will be the leader of a company that ends up being worth
+                            billions of dollars, because I got the answers...I will be the leader of a company that ends up being worth
+                            billions of dollars, because I got the answers...
+                        </Typography>
+                        {/* <form>
+                        <h4>@tnargib</h4>
+                        <p>Login</p>
+                        <hr />
+                        <p>Password</p>
+                        <hr />
+                        <div className="button">Sign in</div>
+                        <small>Forgot password ?</small>
+                        </form> */}
+                    </BackdropFilter>
                 </div>
-            </Parallax>
+            </CardBody>
+                
+            {/* </Parallax> */}
+            </Card>
             
     
          </div>
